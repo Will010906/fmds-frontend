4b17ac7104259e7113c8cb4bf4cfebc9fab463a5
Wilmer Ernesto Lobato <alcantarwil@gmail.com>
2026-07-01 18:09:05 -0600

    update api url to railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://fmds-backend-production.up.railway.app/api',
 })
 
 // Agrega el token automáticamente a cada request
