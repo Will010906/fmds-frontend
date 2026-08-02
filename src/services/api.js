128a392eb0584de9610dfb177c39e56ac96b013e
Wilmer Ernesto Lobato <alcantarwil@gmail.com>
2026-08-01 20:28:31 -0600

    actualizar baseURL en api.js con el nuevo backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://fmds-backend-production.up.railway.app/api',
+  baseURL: 'https://fmds-backend-production-9925.up.railway.app/api',
 })
 
 // Agrega el token automáticamente a cada request
